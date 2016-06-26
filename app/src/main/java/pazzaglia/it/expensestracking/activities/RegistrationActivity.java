0d95d93c832713ecd97e13acf739cbd23bcf0080
Iacopo <iacopo.pazzaglia@hotmail.it>
2016-06-26 23:46:11 +0200

    Dummy login and registration activity

---
+++ app/src/main/java/pazzaglia/it/expensestracking/activities/RegistrationActivity.java
@@ -0,0 +1,19 @@
+package pazzaglia.it.expensestracking.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import pazzaglia.it.expensestracking.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+    }
+
+}
